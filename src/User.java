0bf3b2dc82966f5a5764dfcb5a1bd4c1efe718d2
LoinRe <winard2@mail.ru>
2024-10-28 23:49:17 +0300

    Исправлено ДЗ

--- src/User.java
+++ src/User.java
@@ -3,41 +3,36 @@ public class User {
             throws WrongLoginException, WrongPasswordException {
         // Разрешенные символы
         String allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
-        try {
-            // Проверка логина
-            if (login.length() > 20) {
-                throw new WrongLoginException("Логин не должен превышать 20 символов");
-            }
 
-            for (char c : login.toCharArray()) {
-                if (!isAllowedChar(c, allowedCharacters)) {
-                    throw new WrongLoginException("Логин может содержать только латинские буквы, цифры и знак подчеркивания");
-                }
-            }
-
-            // Проверка пароля
-            if (password.length() > 20) {
-                throw new WrongPasswordException("Пароль не должен превышать 20 символов");
-            }
-
-            for (char c : password.toCharArray()) {
-                if (!isAllowedChar(c, allowedCharacters)) {
-                    throw new WrongPasswordException("Пароль может содержать только латинские буквы, цифры и знак подчеркивания");
-                }
-            }
-
-            // Проверка совпадения паролей
-            if (!password.equals(confirmPassword)) {
-                throw new WrongPasswordException("Пароли не совпадают");
-            }
-
-            // Если все проверки пройдены
-            System.out.println("Пользователь успешно зарегистрирован");
-        } catch (WrongLoginException | WrongPasswordException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        } finally {
-            System.out.println("Проверка завершена"); // Этот блок выполнится всегда
+        // Проверка логина
+        if (login.length() > 20) {
+            throw new WrongLoginException("Логин не должен превышать 20 символов");
         }
+
+        for (char c : login.toCharArray()) {
+            if (!isAllowedChar(c, allowedCharacters)) {
+                throw new WrongLoginException("Логин может содержать только латинские буквы, цифры и знак подчеркивания");
+            }
+        }
+
+        // Проверка пароля
+        if (password.length() > 20) {
+            throw new WrongPasswordException("Пароль не должен превышать 20 символов");
+        }
+
+        for (char c : password.toCharArray()) {
+            if (!isAllowedChar(c, allowedCharacters)) {
+                throw new WrongPasswordException("Пароль может содержать только латинские буквы, цифры и знак подчеркивания");
+            }
+        }
+
+        // Проверка совпадения паролей
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
+
+        // Если все проверки пройдены
+        System.out.println("Пользователь успешно зарегистрирован");
     }
 
     // Метод для проверки, является ли символ допустимым
